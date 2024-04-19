247ea71b1eecc0ce9124fe4106a3a7ef3eb2576f
yunseo choi <160458927+zzzczzzz@users.noreply.github.com>
2024-04-19 20:57:27 +0800

    movie.py
    2 methods for wathc statue added

--- movie.py
+++ movie.py
@@ -14,3 +14,11 @@ class Movie:
     def __str__(self):
         """Create return str for Movie, prinit watched or unwatched refering to watch statue"""
         return f"{self.title} ({self.year}) - {self.category} {'(Watched)' if self.is_watched else '(Unwatched)'}"
+
+    def mark_as_watched(self):
+        """set watched statue to watched"""
+        self.is_watched = True
+
+    def mark_as_unwatched(self):
+        """set watched statue to unwatched"""
+        self.is_watched = False
