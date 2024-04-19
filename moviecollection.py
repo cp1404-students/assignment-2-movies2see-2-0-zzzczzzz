fe0247ad4f7d3f66417fb333d897f449f3d60ff3
yunseo choi <160458927+zzzczzzz@users.noreply.github.com>
2024-04-19 21:11:45 +0800

    moviecollection.py
    imported all needed method, created initial and add movie method

--- moviecollection.py
+++ moviecollection.py
@@ -3,7 +3,28 @@
 
 # TODO: Create your MovieCollection class in this file
 
+from movie import Movie
+import json
+from operator import itemgetter
+
 
 class MovieCollection:
-    """..."""
-    pass
+    def __init__(self):
+        """Initial value for MovieCollection, emtpy string for movies"""
+        self.movies = []
+
+    def add_movie(self, title, year, category):
+        """Add movie, error check each variable, import class Movie for storing"""
+        if title == "" or year == "" or category == "":
+            return "All fields must be completed"
+
+        try:
+            year = int(year)
+        except ValueError:
+            return "Please enter a valid number for the year"
+
+        if category not in ["Action", "Comedy", "Documentary", "Drama", "Fantasy", "Thriller"]:
+            return "Invalid category. Please choose from: Action, Comedy, Documentary, Drama, Fantasy, Thriller"
+
+        movie = Movie(title, year, category)
+        self.movies.append(movie)
