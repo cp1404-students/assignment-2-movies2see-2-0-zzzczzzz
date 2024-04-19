3df1ca26105569e51b0a3d71d6d4377a0af11c5b
yunseo choi <160458927+zzzczzzz@users.noreply.github.com>
2024-04-19 20:51:39 +0800

    movie.py
    create intitial and str

--- movie.py
+++ movie.py
@@ -3,7 +3,14 @@
 
 # TODO: Create your Movie class in this file
 
-
 class Movie:
-    """..."""
-    pass
+    """Initial value for class Movie, set watched statue as fault for default"""
+    def __init__(self, title, year, category, is_watched=False):
+        self.title = title
+        self.year = year
+        self.category = category
+        self.is_watched = is_watched
+
+    def __str__(self):
+        """Create return str for Movie, prinit watched or unwatched refering to watch statue"""
+        return f"{self.title} ({self.year}) - {self.category} {'(Watched)' if self.is_watched else '(Unwatched)'}"
