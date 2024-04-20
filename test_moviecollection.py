4fbdeffc5149df84a2f7c1278faf5fe7f490794b
yunseo choi <160458927+zzzczzzz@users.noreply.github.com>
2024-04-20 23:28:21 +0800

    test_moviecollection.py
    completed tests, modified some parts on moviecollection

--- test_moviecollection.py
+++ test_moviecollection.py
@@ -14,7 +14,7 @@ def run_tests():
 
     # Test loading movies
     print("Test loading movies:")
-    movie_collection.load_movies("movies.json")
+    movie_collection.load_movies()
     print(movie_collection)
     assert movie_collection.movies  # Assuming file is non-empty; non-empty list is considered True
 
@@ -29,9 +29,19 @@ def run_tests():
     print(movie_collection)
     # TODO: Add more sorting tests
 
+    print("Test sorting - title:")
+    movie_collection.sort("title")
+    print(movie_collection)
+
     # TODO: Test saving movies (check file manually to see results)
 
-    # TODO: Add more tests, as appropriate, for each method
+    print("Test saving movies:")
+    movie_collection.save_movies()
 
+
+
+    # TODO: Add more tests, as appropriate, for each method
+    
+    """no more methods to test"""
 
 run_tests()
